5c06186795774b05697ff4ea6e1f056af3f0afdd
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-07-28 13:28:33 +0600

    Add all news card by fecting data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
+import AllNews from "@/components/UI/AllNews";
 
 const HomePage = ({ allNews }) => {
-  console.log(allNews);
   return (
     <>
       <Head>
@@ -16,6 +16,7 @@ const HomePage = ({ allNews }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
+      <AllNews allNews={allNews} />
     </>
   );
 };
