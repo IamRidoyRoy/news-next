a6c17b47d7d696a0f52b3dce027d153de9949395
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-08-25 12:56:08 +0600

    Design product details page

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,11 +1,79 @@
 import RootLayout from "@/components/Layouts/RootLayout";
+import { Col, Row } from "antd";
+import Image from "next/image";
+import {
+  ArrowRightOutlined,
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
 
 const NewsDetailsPage = ({ news }) => {
   return (
     <div>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={12}>
+          <div>
+            <Image
+              src={news?.image_url}
+              width={500}
+              height={300}
+              responsive
+              alt="news image"
+            />
+          </div>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <h1 >{news?.title} </h1>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
 
-      <h1>Title: {news?.title}</h1>
-      <p>Id: {news.id}</p>
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
+
+            <p style={{ fontSize: "20px" }}>
+              {news?.description.length > 100
+                ? news?.description
+                : news?.description}
+            </p>
+          </div>
+        </Col>
+
+      </Row>
+
     </div>
   )
 }
