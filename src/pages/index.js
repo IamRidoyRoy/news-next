5731997d9882414e015da866ed74933594daf8a9
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-08-25 20:48:50 +0600

    Connect redux toolkit with next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
 import AllNews from "@/components/UI/AllNews";
+import { useGetNewsQuery } from "@/redux/api/api";
 
 const HomePage = ({ allNews }) => {
+  const { data, isLoading, isError, error } = useGetNewsQuery();
+  console.log(data)
   return (
     <>
       <Head>
@@ -16,7 +19,7 @@ const HomePage = ({ allNews }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
-      <AllNews allNews={allNews} />
+      <AllNews allNews={data} />
     </>
   );
 };
